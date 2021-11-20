926c855797fd61dc998918d3b7865f5c6b96b1a2
Your Name <you@example.com>
2021-11-21 00:57:07 +0800

    xx

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "build image"
-tag=`date "+%Y%m%d%H%M"`
+tag=`cat Dockerfile  | grep FROM | awk -F/ '{gsub(":","_",$NF);print $NF}'`_`date "+%Y%m%d%H%M"`
 git tag release-v$tag -m $tag
 git push origin release-v$tag
 echo docker pull zhangqijun/image:$tag
