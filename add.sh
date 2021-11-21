20f7365d153d26750db3177c55985b05fc723f57
zhangqijun18 <68327349@qq.com>
2021-11-21 22:43:53 +0800

    build image

--- add.sh
+++ add.sh
@@ -3,6 +3,7 @@ git commit -m "build image"
 tag=`cat Dockerfile  | grep FROM | awk -F/ '{gsub(":","_",$NF);print $NF}'`_`date "+%Y%m%d%H%M"`
 git tag release-v$tag -m $tag
 git push origin release-v$tag
+echo
 echo docker pull zhangqijun/image:$tag
 echo
 echo docker tag zhangqijun/image:$tag `cat Dockerfile | awk '/FROM/{print $2}'` 
