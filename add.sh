b47ec2e006bc93779fc7d01bf8bc9c3a975467b2
zhangqijun18 <68327349@qq.com>
2021-11-21 23:07:11 +0800

    build image

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "build image"
-tag=`cat Dockerfile  | grep FROM | awk -F@ '{print $1}' |awk -F/ '{gsub(":","_",$NF);print $NF}'`_`date "+%Y%m%d%H%M" '`
+tag=`cat Dockerfile  | grep FROM | awk -F@ '{print $1}' | awk -F/ '{gsub(":","_",$NF);print $NF}'`_`date "+%Y%m%d%H%M"`
 git tag release-v$tag -m $tag
 git push origin release-v$tag
 echo
